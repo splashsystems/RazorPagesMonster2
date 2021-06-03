81d29e0be3d3994744097a3c6edc8b4bcbdba5a9
Scot <bscot.hagen@gmail.com>
2021-06-03 15:24:26 -0700

    Added Search by name and by Genre

--- RazorPagesMonster2/Models/Monster.cs
+++ RazorPagesMonster2/Models/Monster.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,13 @@ namespace RazorPagesMonster2.Models
     public class Monster
     {
         public int ID { get; set; }
+        [Display(Name = "Name")]
         public string MonsterName { get; set; }
+        [Display(Name = "Origin Date")]
         [DataType(DataType.Date)]
         public DateTime OriginDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
---
+++ RazorPagesMonster2/Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMonster2.Data;
+
+namespace RazorPagesMonster2.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMonster2Context(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMonster2Context>>()))
+            {
+                // Look for any monsters.
+                if (context.Monster.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Monster.AddRange(
+                    new Monster
+                    {
+                        MonsterName = "Psycho",
+                        OriginDate = DateTime.Parse("1960-9-8"),
+                        Genre = "Crazy Dudes",
+                        Price = 1.25M
+                    },
+
+                    new Monster
+                    {
+                        MonsterName = "Slimer",
+                        OriginDate = DateTime.Parse("1984-6-8"),
+                        Genre = "Ghosts",
+                        Price = 32.00M
+                    },
+
+                    new Monster
+                    {
+                        MonsterName = "Tom(Tom and Jerry)",
+                        OriginDate = DateTime.Parse("1940-7-1"),
+                        Genre = "Cartoon Animal Bullys",
+                        Price = 4.50M
+                    },
+
+                    new Monster
+                    {
+                        MonsterName = "Godzilla",
+                        OriginDate = DateTime.Parse("1954-7-1"),
+                        Genre = "Radioactive Dinosaurs",
+                        Price = 57.00M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
