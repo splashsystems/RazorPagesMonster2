b37da41cfc825b8cab1894e0735c6fc8ab100b30
Scot <bscot.hagen@gmail.com>
2021-06-04 10:15:48 -0700

    Added validations (data annotations)

--- RazorPagesMonster2/Migrations/20210604170721_New_DataAnnotations.Designer.cs
+++ RazorPagesMonster2/Migrations/20210604170721_New_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMonster2.Data;
 
 namespace RazorPagesMonster2.Migrations
 {
     [DbContext(typeof(RazorPagesMonster2Context))]
-    partial class RazorPagesMonster2ContextModelSnapshot : ModelSnapshot
+    [Migration("20210604170721_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
